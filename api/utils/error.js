045e2f8f144fbb42bdf32719d5bf4cc9a669db84
Tolu2312 <agbooladavid9@gmail.com>
2023-11-29 13:29:55 +0100

    create signin api

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,7 @@
 export const errorHandler = (statusCode, message) => {
-    const error = new Error()
+    const error = new Error();
     error.statusCode = statusCode;
-    errorHandler.message = message;
+    error.message = message;
     return error;
-}
+  };
+  
